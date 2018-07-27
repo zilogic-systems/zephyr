2147b120b9a02475dedcb1fdfadc9a9c3974a7e6
raashidtech <raashidmuhammed@zilogic.com>
2018-07-27 11:22:12 +0530

    Fixed the following based on review:
      * Fix proper board name.
      * Add fixme for cmake flash support.
      * Fix irq priority bits number.
      * Fix RC clock frequency.

--- boards/arm/zkit_arm_1769/board.cmake
+++ boards/arm/zkit_arm_1769/board.cmake
@@ -1,10 +1,1 @@
-set_ifndef(LPCLINK_FW jlink)
-
-if(LPCLINK_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
-endif()
-
-board_runner_args(jlink "--device=ZKIT_ARM_1768")
-
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+# FIXME: Add flashing support later
