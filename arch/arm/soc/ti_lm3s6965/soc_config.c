c9deeddd168393d1a9792d84d1146fa03e3d403d
Fadhel Habeeb <fadhel@zilogic.com>
2018-07-31 17:39:53 +0530

    drivers: ethernet: qemu_cortex_m3(ti_lm3s6965)
    1. Added support for Ethernet driver.
    2. Driver mac address is configurable through Kconfig.lm3s6965.
    3. ti_lm3s6965/Kconfig.defconfig is updated to include Ethernet interrupt.
    4. ti_lm3s6965/soc_config.c is updated to activate Ethernet Controller.
    5. ethernet/eth_stats.h is updated to update txn/rxn errors.
    
    Signed-off-by: Fadhel Habeeb <fadhel@zilogic.com>
    Signed-off-by: Nirav Parmar <niravparmar@zilogic.com>

--- arch/arm/soc/ti_lm3s6965/soc_config.c
+++ arch/arm/soc/ti_lm3s6965/soc_config.c
@@ -20,9 +20,9 @@
 
 #define RCGC1 (*((volatile u32_t *)0x400FE104))
 
-#define RCGC1_UART0_EN 0x00000001
-#define RCGC1_UART1_EN 0x00000002
-#define RCGC1_UART2_EN 0x00000004
+#define RCGC1_UART0_EN  0x00000001
+#define RCGC1_UART1_EN  0x00000002
+#define RCGC1_UART2_EN  0x00000004
 
 static int uart_stellaris_init(struct device *dev)
 {
@@ -44,3 +44,20 @@ static int uart_stellaris_init(struct device *dev)
 SYS_INIT(uart_stellaris_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
 
 #endif /* CONFIG_UART_STELLARIS */
+
+#ifdef CONFIG_ETH_LM3S6965
+
+#define RCGC2 (*((volatile u32_t *)0x400FE108))
+
+#define RCGC2_PHY_EN   0x40000000
+#define RCGC2_EMAC_EN  0x10000000
+
+static int eth_stellaris_init(struct device *dev)
+{
+	RCGC2 |= (RCGC2_PHY_EN | RCGC2_EMAC_EN);
+	return 0;
+}
+
+SYS_INIT(eth_stellaris_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+
+#endif /* CONFIG_ETH_LM3S6965 */
