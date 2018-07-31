0b78f301146f529982191db9dbf9ee3287b6f850
Raashid Muhammed <raashidmuhammed@zilogic.com>
2018-07-31 13:20:04 +0530

    lpc17xx: Configure PLL clock
    Configure PLL clock and step up CPU frequency during init.

--- arch/arm/soc/nxp_lpc/lpc17xx/soc.c
+++ arch/arm/soc/nxp_lpc/lpc17xx/soc.c
@@ -14,6 +14,117 @@
 #include <linker/sections.h>
 #include <arch/cpu.h>
 #include <cortex_m/exc.h>
+#include <sys_io.h>
+
+
+static void flash_latency(u32_t frequency)
+{
+	u32_t wait_states;
+
+	wait_states = frequency / 20000000;		/* 1 CLK per 20MHz */
+
+	/* 5 CLKs is the highest reasonable value, works for up to 120MHz */
+	if (wait_states > 5)
+		wait_states = 5;
+	/* set the latency */
+	sys_write32(((wait_states - 1) << FLASHTIM) | RESERVED_value, FLASHCFG);
+}
+
+/* Power-up System Osc. */
+static void osc_sel(void)
+{
+	u32_t value;
+
+	value = sys_read32(PCLKSEL1);
+	value |= PCLKSEL1_PCLK_GPIOINT_DIV1  |
+		 PCLKSEL1_PCLK_PCB_DIV1      |
+		 PCLKSEL1_PCLK_SYSCON_DIV1;
+	sys_write32(value, PCLKSEL1);
+	sys_clear_bit(SCS, OSCRANGE); /* Clock source Rannge as 20Mhz */
+	sys_set_bit(SCS, OSCEN);      /* Enable Main OSC */
+	flash_latency(CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
+	 /* Wait for main OSC to start up */
+	while (sys_test_bit(SCS, OSCSTAT) == 0);
+	/* Clock soure selection as main oscillator */
+	sys_set_bit(CLKSRCSEL, 0);
+	/* Clock soure selection as main oscillator */
+	sys_clear_bit(CLKSRCSEL, 1);
+
+}
+
+static void pll0_feed(void)
+{
+	sys_write32(PLL0FEED_FIRST, PLL0FEED);
+	sys_write32(PLL0FEED_SECOND, PLL0FEED);
+}
+
+static void pll_config(u32_t crystal, u32_t frequency)
+{
+	u32_t prediv;
+	u32_t mul;
+	u32_t corediv;
+	u32_t fcco;
+	u32_t core_frequency;
+	u32_t best_prediv = 0;
+	u32_t best_mul = 0;
+	u32_t best_corediv = 0;
+	u32_t best_core_frequency = 0;
+
+	/* NSEL0 in [1; 32] */
+	for (prediv = 1; prediv <= 32; prediv++)
+		/* MSEL0 in [5; 512] */
+		for (mul = 6; mul <= 512; mul++) {
+			/* calculate PLL output frequency */
+			fcco = 2 * mul * crystal / prediv;
+
+			/* skip invalid settings - fcco must be in
+			 * [275M; 550M]
+			 */
+			if ((fcco < 275000000) || (fcco > 550000000))
+				continue;
+
+			/*CCLKSEL in [1; 256] */
+			for (corediv = 1; corediv <= 256; corediv++) {
+				/* calculate core frequency */
+				core_frequency = fcco / corediv;
+
+				/* skip frequencies above desired value */
+				if (core_frequency > frequency)
+					continue;
+
+				/* is this configuration better than
+				 * previous one?
+				 */
+				if (core_frequency > best_core_frequency) {
+					/* save values */
+					best_core_frequency = core_frequency;
+					best_prediv = prediv;
+					best_mul = mul;
+					best_corediv = corediv;
+
+					/* is this configuration "perfect"? */
+					if (core_frequency == frequency)
+						break;
+				}
+			}
+		}
+
+	/* set NSEL0 and MSEL0 */
+	sys_write32(((best_prediv - 1) << PLL0CFG_NSEL0) |
+		    ((best_mul - 1) << PLL0CFG_MSEL0_0_bit), PLL0CFG);
+	pll0_feed();			/* validate change in PLL0CFG */
+	sys_set_bit(PLL0CON, PLLE0);
+	pll0_feed();			/* validate change in PLL0CON */
+	/* set core clock divider CCLKSEL */
+	sys_write32((best_corediv - 1) << CCLKSEL_bit, CCLKCFG);
+	 /* wait for PLL0 lock */
+	while (sys_test_bit(SCS, OSCSTAT) == 0);
+	/* connect PLL0 as clock source */
+	sys_set_bit(PLL0CON, PLLC0);
+	pll0_feed();	       /* validate connection */
+
+}
+
 
 /**
  *
@@ -25,7 +136,8 @@
 
 static ALWAYS_INLINE void clkInit(void)
 {
-	/* fixme: Initialize and configure the clock. */
+	osc_sel();
+	pll_config(CONFIG_SYS_CRYSTAL_FREQ, CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
 }
 
 /**
--- arch/arm/soc/nxp_lpc/lpc17xx/soc.h
+++ arch/arm/soc/nxp_lpc/lpc17xx/soc.h
@@ -20,8 +20,63 @@ extern "C" {
 #endif
 
 #ifndef _ASMLANGUAGE
-#include <device.h>
-#include <misc/util.h>
+
+enum sysconfig_reg {
+
+	CLKSRCSEL = 0x400FC10C,
+	PLL0CON = 0x400FC080,
+	PLL0CFG = 0x400FC084,
+	PLL0FEED = 0x400FC08C,
+	CCLKCFG = 0x400FC104,
+	PCLKSEL1 = 0x400FC1AC,
+	SCS = 0x400FC1A0,
+	FLASHCFG = 0x400FC000
+};
+
+enum scs_config {
+	OSCRANGE = 4,
+	OSCEN = 5,
+	OSCSTAT = 6
+};
+
+/* This enums used for enable clock to peripheral, and
+ * here we use PCLKSEL1_PCLK_FIRST enum to make each enum
+ * value as unique.
+ */
+enum pclksel_config {
+	PCLKSEL1_PCLK_FIRST             = 100,
+	PCLKSEL1_PCLK_GPIOINT_DIV1	= 2 + PCLKSEL1_PCLK_FIRST,
+	PCLKSEL1_PCLK_PCB_DIV1		= 4 + PCLKSEL1_PCLK_FIRST,
+	PCLKSEL1_PCLK_SYSCON_DIV1	= 28 + PCLKSEL1_PCLK_FIRST,
+};
+
+enum flashcfg_config {
+	FLASHTIM = 12,
+	RESERVED_value = 0x3A
+};
+
+enum pll0cfg_config {
+	PLL0CFG_MSEL0_0_bit = 0,
+	PLL0CFG_NSEL0 = 16,
+	PLL0CFG_MSEL0 = (1 << PLL0CFG_MSEL0_0_bit)
+};
+
+
+enum pllofeed_config {
+
+	PLL0FEED_FIRST	= 0xAA,
+	PLL0FEED_SECOND	= 0x55
+};
+
+enum cclkcfg_config {
+	CCLKSEL_bit = 0
+};
+
+enum pll0con_config {
+	PLLE0 = 0,
+	PLLC0 = 1
+};
+
 #endif /* !_ASMLANGUAGE */
 
 #ifdef __cplusplus
