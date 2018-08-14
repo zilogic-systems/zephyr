67c906841719e0524cb3f7de7b5c7d26e9d3de9d
Fadhel Habeeb <fadhel@zilogic.com>
2018-08-14 12:58:18 +0530

    UART driver 	* Enabled UART0 in Txn mode 	* printk connected to UART0 	* Tested with samples/helloworld

--- arch/arm/soc/nxp_lpc/lpc17xx/soc.c
+++ arch/arm/soc/nxp_lpc/lpc17xx/soc.c
@@ -16,6 +16,38 @@
 #include <cortex_m/exc.h>
 #include <sys_io.h>
 
+static void nxp_lpc17xx_uart_init(void)
+{
+	u32_t regval;
+	/* Step 1: Enable power on UART0 
+	 * UART0 is enabled on reset
+	 */
+	regval = sys_read32(LPC17XX_SYSCON_PCONP);
+	regval &= ~SYSCON_PCLKSEL0_UART0_MASK;
+	sys_write32(regval, LPC17XX_SYSCON_PCONP);
+
+	/* Step 2: Enable clocking on UART 
+	 * PCCLK_peripheral = CCLK
+	 */
+	regval = sys_read32(LPC17XX_SYSCON_PCLKSEL0);
+	regval &= ~SYSCON_PCLKSEL0_UART0_MASK;
+	regval |= 0x01 << SYSCON_PCLKSEL0_UART0_SHIFT;
+	sys_write32(regval, LPC17XX_SYSCON_PCLKSEL0);
+
+	/* Configure TXD pin */
+	regval = sys_read32(LPC17XX_PINSEL0);
+	regval &= ~SYSCON_PINSEL0_TXD0_MASK;
+	regval |= 0x01 << SYSCON_PINSEL0_TXD0_SHIFT;
+	sys_write32(regval, LPC17XX_PINSEL0);
+
+	/* Configure RXD pin */
+	regval = sys_read32(LPC17XX_PINSEL0);
+	regval &= ~SYSCON_PINSEL0_RXD0_MASK;
+	regval |= 0x01 << SYSCON_PINSEL0_RXD0_SHIFT;
+	sys_write32(regval, LPC17XX_PINSEL0);
+
+	/* Pin mode is pull-up by default */
+}
 
 static void flash_latency(u32_t frequency)
 {
@@ -173,6 +205,9 @@ static int nxp_lpc17xx_init(struct device *arg)
 
 	/* restore interrupt state */
 	irq_unlock(oldLevel);
+
+	/* Initialize UART() */
+	nxp_lpc17xx_uart_init();
 
 	return 0;
 }
