52f9b1f3ea7c618e4bdab17e957bb6252f5b1599
Fadhel Habeeb <fadhel@zilogic.com>
2018-09-26 12:08:50 +0530

    UART Driver for lpc17XX
     Driver support UART0. UART0 is connected to console
     and as such supports printk and debugging.
     Zephyr STDIO console APIs are supported.
     RX/TX Fifo lengths are 1 character.

--- drivers/serial/uart_lpc17xx.c
+++ drivers/serial/uart_lpc17xx.c
@@ -4,10 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @brief UART Driver for lpc17XX
+ *
+ * Driver support UART0. UART0 is connected to console
+ * and as such supports printk and debugging.
+ * Zephyr STDIO console APIs are supported.
+ */
+
 #include <uart.h>
+
+/*UART Device data structure */
+struct uart_lpc17xx_dev_data {
+	u32_t baud_rate;			/*Baud rate */
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	uart_irq_callback_user_data_t cb;	/*Callback function pointer */
+	void *cb_data;				/* Callback function argument */
+#endif
+};
 
 #define DEV_CFG(dev) \
 	((struct uart_device_config *)(dev)->config->config_info)
+
+#define DEV_DATA(dev) \
+	((struct uart_lpc17xx_dev_data *)(dev)->driver_data)
 
 /* Register Mapping */
 #define LPC17XX_UART_RBR	((DEV_CFG(dev)->base) + 0x000)
@@ -15,82 +35,244 @@
 #define LPC17XX_UART_DLL	((DEV_CFG(dev)->base) + 0x000)
 #define LPC17XX_UART_DLM	((DEV_CFG(dev)->base) + 0x004)
 #define LPC17XX_UART_IER	((DEV_CFG(dev)->base) + 0x004)
+#define LPC17XX_UART_IIR	((DEV_CFG(dev)->base) + 0x008)
 #define LPC17XX_UART_FCR	((DEV_CFG(dev)->base) + 0x008)
 #define LPC17XX_UART_LCR	((DEV_CFG(dev)->base) + 0x00c)
 #define LPC17XX_UART_LSR	((DEV_CFG(dev)->base) + 0x014)
 
-/* UART LCR Reg Bit fields set value*/
-#define BIT_LCR_WL		0x03 /* 8 bit character len */
-#define BIT_LCR_DLAB		(1 << 7)  /* Bit 7: Divisor Latch Access bit */
-
-/* UART FCR Reg bit field set vale */
-#define BIT_FCR_FIFOEN		(1 << 0)  /* Bit 0:  Enable FIFOs */
-
-/* UART LSR Reg bit fields */
-#define BIT_LSR_THRE           (1 << 5)  /* Bit 5:  Transmitter Holding Register empty */
-
 static int uart_lpc17xx_init(struct device *dev)
 {
 	uint16_t dl;
-	uint32_t regval;
 
-	/* Set 8-bits char, DLAB=1 */
-	regval = sys_read32(LPC17XX_UART_LCR);
-	regval |= BIT_LCR_WL | BIT_LCR_DLAB;
-	sys_write32(regval, LPC17XX_UART_LCR);
+	/* Set 8-bits char*/
+	sys_write32(0x03, LPC17XX_UART_LCR);
 
-	/* Write DL value i.e = PCLK / BAUD / 16
-	 * 120 MHz / 115200 / 16
+	/* Set DLAB=1, Write DL value i.e = PCLK / BAUD / 16
+	 * 120 MHz / 115200 / 16, Finally set DLAB = 0
 	 */
+	sys_set_bit(LPC17XX_UART_LCR, 7);
 	dl = 65;
-	sys_write32(dl >> 8, LPC17XX_UART_DLM);
-	sys_write32(dl & 0xff, LPC17XX_UART_DLL);
+	sys_write8(dl >> 8, LPC17XX_UART_DLM);
+	sys_write8(dl & 0xff, LPC17XX_UART_DLL);
+	sys_clear_bit(LPC17XX_UART_LCR, 7);
 
-	/* Clear  DLAB = 0 */
-	regval = sys_read32(LPC17XX_UART_LCR);
-	regval &= ~BIT_LCR_DLAB;
-	sys_write32(regval, LPC17XX_UART_LCR);
+	/* Enable FIFO,
+	 * Rx Trigger level is one character in FIFO
+	 */
+	sys_set_bit(LPC17XX_UART_FCR, 0);
 
-	/* Enable FIFO */
-	regval = sys_read32(LPC17XX_UART_FCR);
-	regval |= BIT_FCR_FIFOEN;
-	sys_write32(regval, LPC17XX_UART_FCR);
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	DEV_CFG(dev)->irq_config_func(dev);
+#endif
+	return 0;
 }
 
-/*
- * FIXME: Receive needs to be implemented.
- */
 static int uart_lpc17xx_poll_in(struct device *dev, unsigned char *c)
 {
-	return -ENOTSUP;
+	while (!sys_test_bit(LPC17XX_UART_LSR, 0)) {
+		/* FIFO is empty and no character to read */
+		return -1;
+	}
+
+	/* Read the character */
+	*c = sys_read8(LPC17XX_UART_RBR);
+	return 0;
 }
 
 static unsigned char uart_lpc17xx_poll_out(struct device *dev, unsigned char c)
 {
-	while ((sys_read32(LPC17XX_UART_LSR) & BIT_LSR_THRE) == 0)
+	/* Check if transmitter is empty,
+	 * and write a character to data reg when empty.
+	 */
+	while (!sys_test_bit(LPC17XX_UART_LSR, 5))
 		;
 
 	/* Send the character */
-	sys_write32(c, LPC17XX_UART_THR);
+	sys_write8(c, LPC17XX_UART_THR);
+
+	return c;
+
+/*FIXME: To send a character when hardware flow control is enabled,
+ * The handshake signal CTS must be asserted
+ */
 }
+
+static int uart_lpc17xx_err_check(struct device *dev)
+{
+	int err = 0;
+
+	if (sys_test_bit(LPC17XX_UART_LSR, 1)) {
+		err |= UART_ERROR_OVERRUN;
+	}
+
+	if (sys_test_bit(LPC17XX_UART_LSR, 2)) {
+		err |= UART_ERROR_PARITY;
+	}
+
+	if (sys_test_bit(LPC17XX_UART_LSR, 3)) {
+		err |= UART_ERROR_FRAMING;
+	}
+
+	return err;
+}
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+
+static inline int uart_lpc17xx_irq_fifo_fill(struct device *dev, u8_t *tx_data,
+				  int data_len)
+{
+	u8_t num_tx = 0;
+
+	while ((data_len - num_tx > 0) && sys_test_bit(LPC17XX_UART_LSR, 5)) {
+		sys_write8(tx_data[num_tx++], LPC17XX_UART_THR);
+	}
+
+	return num_tx;
+}
+
+static inline int uart_lpc17xx_irq_fifo_read(struct device *dev, u8_t *rx_data,
+				 const int data_len)
+{
+	u8_t num_rx = 0;
+
+	while ((data_len - num_rx > 0) && sys_test_bit(LPC17XX_UART_LSR, 0)) {
+		rx_data[num_rx++] = sys_read8(LPC17XX_UART_RBR);
+	}
+
+	return num_rx;
+}
+
+static inline void uart_lpc17xx_irq_tx_enable(struct device *dev)
+{
+	sys_set_bit(LPC17XX_UART_IER, 1);
+}
+
+static inline void uart_lpc17xx_irq_tx_disable(struct device *dev)
+{
+	sys_clear_bit(LPC17XX_UART_IER, 1);
+}
+
+static inline int uart_lpc17xx_irq_tx_ready(struct device *dev)
+{
+	return sys_test_bit(LPC17XX_UART_LSR, 5);
+}
+
+static inline void uart_lpc17xx_irq_rx_enable(struct device *dev)
+{
+	sys_set_bit(LPC17XX_UART_IER, 0);
+}
+
+static inline void uart_lpc17xx_irq_rx_disable(struct device *dev)
+{
+	sys_clear_bit(LPC17XX_UART_IER, 0);
+}
+
+static inline int uart_lpc17xx_irq_rx_ready(struct device *dev)
+{
+	return sys_test_bit(LPC17XX_UART_LSR, 0);
+}
+
+static inline void uart_lpc17xx_irq_err_enable(struct device *dev)
+{
+	sys_set_bit(LPC17XX_UART_IER, 2);
+}
+
+static void uart_lpc17xx_irq_err_disable(struct device *dev)
+{
+	sys_clear_bit(LPC17XX_UART_IER, 2);
+}
+
+static int uart_lpc17xx_irq_is_pending(struct device *dev)
+{
+	return !sys_test_bit(LPC17XX_UART_IIR, 0);
+}
+
+static int uart_lpc17xx_irq_update(struct device *dev)
+{
+	return 1;
+}
+
+static void uart_lpc17xx_irq_callback_set(struct device *dev,
+					  uart_irq_callback_user_data_t cb,
+					  void *cb_data)
+{
+	struct uart_lpc17xx_dev_data *dev_data = DEV_DATA(dev);
+
+	dev_data->cb = cb;
+	dev_data->cb_data = cb_data;
+}
+
+static void uart_lpc17xx_isr(void *arg)
+{
+	struct device *dev = arg;
+	struct uart_lpc17xx_dev_data *dev_data = DEV_DATA(dev);
+
+	if (dev_data->cb) {
+		dev_data->cb(dev_data->cb_data);
+	}
+}
+
+#endif /*CONFIG_UART_INTERRUPT_DRIVEN */
 
 static const struct uart_driver_api uart_lpc17xx_driver_api = {
 	.poll_in = uart_lpc17xx_poll_in,
 	.poll_out = uart_lpc17xx_poll_out,
+	.err_check = uart_lpc17xx_err_check,
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	.fifo_fill = uart_lpc17xx_irq_fifo_fill,
+	.fifo_read = uart_lpc17xx_irq_fifo_read,
+	.irq_tx_enable = uart_lpc17xx_irq_tx_enable,
+	.irq_tx_disable = uart_lpc17xx_irq_tx_disable,
+	.irq_tx_ready = uart_lpc17xx_irq_tx_ready,
+	.irq_rx_enable = uart_lpc17xx_irq_rx_enable,
+	.irq_rx_disable = uart_lpc17xx_irq_rx_disable,
+	.irq_rx_ready = uart_lpc17xx_irq_rx_ready,
+	.irq_err_enable = uart_lpc17xx_irq_err_enable,
+	.irq_err_disable = uart_lpc17xx_irq_err_disable,
+	.irq_is_pending = uart_lpc17xx_irq_is_pending,
+	.irq_update = uart_lpc17xx_irq_update,
+	.irq_callback_set = uart_lpc17xx_irq_callback_set,
+#endif
 };
 
 #ifdef CONFIG_UART_LPC17XX_PORT_0
 
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+static void irq_config_func_0(struct device *dev);
+#endif
+
 static struct uart_device_config uart0_cfg = {
-	.base = (u8_t *)CONFIG_UART0_BASE_ADDR,
+	.base = (unsigned int)CONFIG_UART0_BASE_ADDR,
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	.irq_config_func = irq_config_func_0,
+#endif
+};
+
+static struct uart_lpc17xx_dev_data uart_lpc17xx_dev_data_0 = {
+	.baud_rate = CONFIG_UART0_BAUD_RATE,
 };
 
 DEVICE_AND_API_INIT(lpc17xx_uart_0, CONFIG_UART0_LABEL,
 		    &uart_lpc17xx_init,
-		    NULL, &uart0_cfg,
+		    &uart_lpc17xx_dev_data_0, &uart0_cfg,
 		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &uart_lpc17xx_driver_api);
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+static void irq_config_func_0(struct device *dev)
+{
+	IRQ_CONNECT(CONFIG_UART0_IRQ,
+		    CONFIG_UART0_IRQ_PRI,
+		    uart_lpc17xx_isr, DEVICE_GET(lpc17xx_uart_0),
+		    0);
+	irq_enable(CONFIG_UART0_IRQ);
+	uart_lpc17xx_irq_rx_enable(dev);
+
+}
+#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
 
 #endif /* CONFIG_UART_LPC17XX_PORT_0 */
 
 /* FIXME: Enable UART 1/2/3 */
+
